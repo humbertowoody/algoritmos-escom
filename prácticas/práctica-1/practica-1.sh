34fdced6ae7c3650453e6dc5e6fc873e52c1ab35
Humberto Alcocer <humbertowoody@gmail.com>
2022-03-29 00:41:19 -0600

    feat: add timeout optimization for script

--- prácticas/práctica-1/practica-1.sh
+++ prácticas/práctica-1/practica-1.sh
@@ -14,7 +14,7 @@ ARCHIVO_10_MILLONES_DE_NUMEROS=10_millones_numeros.txt
 
 # Bandera para indicar si queremos una ejecución en paralelo (resultados no
 # válidos pero excelente para hacer pruebas).
-EJECUCION_EN_PARALELO=true
+EJECUCION_EN_PARALELO=false
 
 # Nombre del programa para hacer "timeout", es decir, que cuide que si nuestro
 # binario toma más de X tiempo, lo finaliza.
@@ -23,10 +23,11 @@ EJECUCION_EN_PARALELO=true
 # $ brew install coreutils
 # Y, para no reemplazar las utilidades default de macOS (por razones obvias), los binarios
 # incluídos se inician con una 'g'. Por ejemplo 'timeout' -> 'gtimeout'.
-PROGRAMA_TIMEOUT=gtimeout
+# En Ubuntu está incluída la utilidad original: timeout.
+PROGRAMA_TIMEOUT=timeout
 
 # Máximo tiempo de ejecución por programa.
-TIEMPO_TIMEOUT=1h
+TIEMPO_TIMEOUT=10s
 
 # Índices de los algoritmos disponibles en el programa.
 ALGORITMOS=(
@@ -44,27 +45,26 @@ ALGORITMOS=(
 
 # Arreglo con los valores de n para probar.
 VALORES_N=(
-  # 100
-  # 1000
-  # 5000
-  # 10000
-  # 50000
-  # 100000
-  # 200000
-  # 400000
-  # 600000
-  # 800000
-  # 1000000
-  # 2000000
-  # 3000000
-  # 4000000
-  # 5000000
-  # 6000000
-  # 7000000
-  # 8000000
-  # 9000000
-  # 10000000
-  500000
+  100
+  1000
+  5000
+  10000
+  50000
+  100000
+  200000
+  400000
+  600000
+  800000
+  1000000
+  2000000
+  3000000
+  4000000
+  5000000
+  6000000
+  7000000
+  8000000
+  9000000
+  10000000
 );
 
 # Generamos el nombre del archivo para los resultados.
@@ -78,12 +78,18 @@ ARCHIVO_CSV=$CARPETA_RESULTADOS/res-$(date +"%H-%M_%d_%m_%Y")-to-$TIEMPO_TIMEOUT
 # 3) Iteramos sobre cada algoritmo seleccionado.
 # 4) Limpiar binarios y archivos objeto.
 
+# Variable auxiliar para mejorar el rendimiento llevando el tracking de los
+# algoritmos que ya dieron timeout para omitirlos de las pruebas.
+declare -A timeouts;
+for alg in ${ALGORITMOS[@]}; do
+  timeouts[$alg]=false;
+done
+
 # Compilamos el programa.
 make all;
 
 # Imprimimos el encabezado CSV estándar para la práctica.
 echo "algoritmo,n,tiempo_real,tiempo_usuario,tiempo_sistema,tiempo_cpu_wall" >$ARCHIVO_CSV;
-
 # Mensaje de inicio.
 echo "Inicio de ejecución: $(date)";
 echo "Máximo tiempo de ejecución: $TIEMPO_TIMEOUT";
@@ -104,14 +110,20 @@ for n in ${VALORES_N[@]}; do
   echo "Iniciamos ejecución para n=${n}.";
   # Iteramos sobre los algoritmos seleccionados.
   for alg in ${ALGORITMOS[@]}; do
-    # Aquí decidimos si queremos ejecutar los algoritmos en paralelo o secuencialmente.
-    if [ "$EJECUCION_EN_PARALELO" = true ] ; then
-      # Ejecutamos cada algoritmo en el background.
-      $PROGRAMA_TIMEOUT $TIEMPO_TIMEOUT ./$EJECUTABLE $alg $n <$ARCHIVO_10_MILLONES_DE_NUMEROS >>$ARCHIVO_CSV &
+    # Verificamos si el algoritmo en cuestión ya dió timeout en una iteración 
+    # anterior y, de ser así, lo omitimos.
+    if [ "${timeouts[$alg]}" = false ]; then
+      # Aquí decidimos si queremos ejecutar los algoritmos en paralelo o secuencialmente.
+      if [ "$EJECUCION_EN_PARALELO" = true ] ; then
+        # Ejecutamos cada algoritmo en el background.
+        $PROGRAMA_TIMEOUT $TIEMPO_TIMEOUT ./$EJECUTABLE $alg $n <$ARCHIVO_10_MILLONES_DE_NUMEROS >>$ARCHIVO_CSV || { echo "Timeout para \"$alg\" en complejidad: $n ($(date))" && timeouts[$alg]=true; } &
+      else
+        # Ejecutamos cada algoritmo de forma secuencial.
+        echo "Ejecutando algoritmo: \"$alg\" ($(date))";
+        $PROGRAMA_TIMEOUT $TIEMPO_TIMEOUT ./$EJECUTABLE $alg $n <$ARCHIVO_10_MILLONES_DE_NUMEROS >>$ARCHIVO_CSV || { echo "Timeout para \"$alg\" en complejidad: $n ($(date))" && timeouts[$alg]=true; };
+     fi
     else
-      # Ejecutamos cada algoritmo de forma secuencial.
-      echo "Ejecutando algoritmo: \"$alg\" ($(date))";
-      $PROGRAMA_TIMEOUT $TIEMPO_TIMEOUT ./$EJECUTABLE $alg $n <$ARCHIVO_10_MILLONES_DE_NUMEROS >>$ARCHIVO_CSV;
+      echo "Saltando algoritmo: \"$alg\" para n=$n por timeout previo.";
     fi
   done
 done
